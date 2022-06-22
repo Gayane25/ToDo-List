8158cd7b39c749d96401e9d46eb4d5b5c63f55e2
Gayane25 <gayabennu@gmail.com>
2022-06-22 11:22:10 +0400

    feat:added react-router and ordered files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {Routes,Route} from "react-router-dom";
+import All from './pages/All';
+import Active from './pages/Active';
+import Completed from './pages/Completed';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Routes>
+     <Route path='/' element={<All/>} />
+     <Route path='/active' element={<Active/>}/>
+     <Route path='completed' element={<Completed />}/>
+    </Routes>
   );
 }
 

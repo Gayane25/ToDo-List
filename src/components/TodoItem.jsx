c701d4adf9928fb22c5a7c08df240e8cc640783b
Gayane25 <gayabennu@gmail.com>
2022-06-23 14:07:21 +0400

    feat:added sweet alert for alert message

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useTodoContext, ACTION_TYPES} from "../state/initialState";
 import {ItemStyled} from "./TodoItemStyled";
 import bin from "../assets/61-trash.svg"
+import Swal from 'sweetalert2';
 
 function TodoItem({item}) {
+
+   const showingCompleted =()=>{
+      Swal.fire({
+        toast: true,
+        icon: 'info',
+        title: 'your action has successfully done',
+        // animation: false,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        // didOpen: (toast) => {
+        //   toast.addEventListener('mouseenter', Swal.stopTimer)
+        //   toast.addEventListener('mouseleave', Swal.resumeTimer)
+        // }
+      })
+    }
    
     const {state, dispatch} =useTodoContext();
   console.log(state)
+
     const handleCompleted =()=>{
-        dispatch({type:ACTION_TYPES.MAKE_COMPLETED, payload:{id:item.id}})
+        dispatch({type:ACTION_TYPES.MAKE_COMPLETED, payload:{id:item.id}});
+        showingCompleted();
+       
     }
     const handleDelete =()=>{
         dispatch({type:ACTION_TYPES.DELETE_TODO, payload:{id:item.id}})
     }
+
    
   return (
     <ItemStyled>
@@ -21,8 +43,7 @@ function TodoItem({item}) {
        <label style={item.isCompleted ? {textDecoration: "line-through"}:null} htmlFor={item.description}>{item.description}
         </label>
       </div>
-      
-        {item.isCompleted && (<span onClick={handleDelete}><img src ={bin} alt ="recycle bin"/></span>)}
+        {item.isCompleted &&<span onClick={handleDelete}><img src ={bin} alt ="recycle bin"/></span>}
     </ItemStyled>
   )
 }
--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoItem from '../components/TodoItem';
 import {useTodoContext, ACTION_TYPES} from "../state/initialState";
 import { DeleteButton } from '../components/DeleteButtonStyled';
@@ -6,7 +6,7 @@ import bin from "../assets/61-trash-white.svg"
 
 function Completed() {
     const {state, dispatch} = useTodoContext();
-
+  const [recyclebin, setRecycleBin] = useState(false);
     const deleteAll =()=>{
         dispatch({type:ACTION_TYPES.DELETE_ALL})
     }
@@ -14,7 +14,7 @@ function Completed() {
   return (
     <div>
         { 
-            state.todos.filter((item)=>item.isCompleted ===true).map((item)=><TodoItem key ={item.id} item={item}/>)
+            state.todos.filter((item)=>item.isCompleted ===true).map((item)=><TodoItem key ={item.id} item={item} setRecycleBin={setRecycleBin}/>)
         }
         <div style={{textAlign:"end"}}>
          <DeleteButton onClick={deleteAll}><img src={bin} alt ="delete"/> Delete all</DeleteButton>
